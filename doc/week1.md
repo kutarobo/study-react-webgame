f656049ab29d621b8566c2e8f5264e7f7dc44ce2
kutarobo <kutarobo@naver.com>
2021-07-25 04:51:30 +0900

    feat: Add chapter2 and package.json

--- doc/week1.md
+++ doc/week1.md
@@ -27,3 +27,55 @@ this.state = {
 }
 
 ```
+
+## class vs hooks
+- react에서는 hooks 를 추천한다.
+- 코드가 더 간결해진다.
+- hooks: state가 변하면 컴포넌트 함수가 재실행하기때문에 조금 더 느릴 수 있다. (ex. gugudan 전체)
+- class: state가 변하면 reder() 함수만 재실행한다 (ex. gugudan 의 render)
+
+### useRef
+1. class 방식에서
+```js
+class GuGuDan extends React.Component {
+    input;
+
+    this.input.focus();
+    reder() {
+        return (<input ref={(c) => { this.input = c; }} />) // 
+    }
+}
+```
+
+2. hook 방식에서
+```js
+
+const GuGuDan = () => {
+    const inputRef = React.useRef(초기값);    
+    inputRef.current.focus();   // current가 중간에 들어가줘야함.
+    return (<input ref={inputRef} />)
+}
+
+```
+
+## package.json 설치
+```js
+npm init
+npm i react react-dom
+npm i -D webpack webpack-cli
+
+npm i -D @babel/core    // 바벨의 기본적인것
+npm i -D @babel/preset-env // 옛날문법 지원
+npm i -D @babel/preset-react // 리액트같은거 지원함.
+npm i -D babel-loader // 바벨과 웹팩연결
+npm i -D @babel/plugin-proposal-class-properties
+
+npm i -D react-refresh @pmmmwh/react-refresh-webpack-plugin // 핫로딩
+npm i -D webpack-dev-server
+```
+-D 옵션: 개발용으로 사용 (devDependencies)
+
+
+- preset: plugin 들의 모음
+
+github.com/browerslist
---
+++ lecture/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');   // node 설치시 제공된다.
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development', // 실서비스: production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx'], // 생략가능
+    },
+
+    // 여기부터 중요
+    entry: {
+        app: ['./client',], // 이것들을 합쳐서 아래 app.js 로 만들어준다. 'WordRelay.jsx' 는 다른데에서 불러왔기때문에 여기에서 추가할 필요가 없다.
+    }, // 입력
+    module: {
+        rules: [{
+            test: /\.jsx?/, // js, jsx 룰을 적용한다. (정규표현식으로 js, jsx 타겟)
+            loader: 'babel-loader', // 그룰은 바벨로더 룰
+            options: {
+                presets: [
+                    [
+                        '@babel/preset-env', 
+                        {
+                            targets: {
+                                browsers: ['> 5% in KR', 'last 2 chrome versions'], // 상위 5% 점유율을 가진 브라우져 지원, 마지막 2버젼의 크롬지원.
+                            },
+                            debug: true,
+                        }
+                    ], 
+                    '@babel/preset-react'
+                ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel',  // 핫로딩관련
+                ],
+            }
+        }]
+    },    // entry 파일을 읽어서 modules을 적용하고 output으로 결과물을 뺀다.
+    plugins: [
+        new RefreshWebpackPlugin()
+    ],    // 확장프로그램
+    output: {
+        path: path.join(__dirname, 'dist'), //  __dirname == 현재폴더. 안의 dist.
+        filename: 'app.js',
+        publicPath: '/dist/',
+    }, // 출력
+    devServer: {
+        publicPath: '/dist/',   // 메모리로 이경로에 저장해둠
+        hot: true,
+    },
+}
