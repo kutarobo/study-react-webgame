04aa169c36350e98e2e17f3dfd36a9f777df260a
kutarobo <kutarobo@naver.com>
2021-07-25 09:25:33 +0900

    feat: Add chapter5

---
+++ 4.반응속도체크/ResponseCheck.jsx
@@ -0,0 +1,32 @@
+ import React, {Component} from 'react';
+
+ class ResponseCheck extends Component {
+
+    state = {
+        state: 'waiting',
+        message: '클릭해서 시작하세요',
+        result: [],
+    };
+
+    onClickScreen = () => {
+
+    };
+
+    render( ){
+        return (
+            <>
+                <div
+                    id="screen"
+                    className={this.state.state}
+                    onClick={this.onClickScreen}
+                
+                >
+                    {this.state.message}
+                </div>
+                <div>평균시간: {this.state.result.reduce((a,c) => a + c)/ this.state.result.length}ms</div>
+            </>
+        )
+    }
+ }
+
+ export default ResponseCheck
